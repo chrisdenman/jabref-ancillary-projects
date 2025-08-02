0ce10068f9c6b1f1c71f67f3381c00d8aad31a21
Chris Denman <chrisdenman@icloud.com>
2025-08-02 12:43:35 +0100

    Removed a @todo, added to the README.md as points to investigate.

--- reproduction/src/main/java/Main.java
+++ reproduction/src/main/java/Main.java
@@ -2,7 +2,6 @@ import org.jabref.gui.desktop.os.Linux;
 import org.jabref.logic.util.HeadlessExecutorService;
 
 public class Main {
-    // @todo why doesn't gradle return immediately now
     public static void main(String[] args) {
         Linux.nativeOpenFile(args[0]);
         HeadlessExecutorService.INSTANCE.shutdownEverything();
