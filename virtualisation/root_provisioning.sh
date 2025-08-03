4ed756337c8c686b161f8ac77e21bf2c30af1e14
Chris Denman <chrisdenman@icloud.com>
2025-08-03 12:18:41 +0100

    Installing gnome-vfs2 on opensuse 15.6 now which means Desktop.open works.

--- virtualisation/root_provisioning.sh
+++ virtualisation/root_provisioning.sh
@@ -7,7 +7,9 @@ case $1 in
   openSUSE)
     zypper -n refresh
     zypper -n update
-    zypper -n install kernel-devel gcc make patterns-kde-kde sddm virtualbox-guest-tools zip unzip konsole dolphin okular xdg-utils firefox
+    zypper addrepo --no-gpgcheck https://download.opensuse.org/repositories/zypp:SLE-15-SP6-Branch/openSUSE_Leap_15.6/zypp:SLE-15-SP6-Branch.repo
+
+    zypper -n install kernel-devel gcc make patterns-kde-kde sddm virtualbox-guest-tools zip unzip konsole dolphin okular xdg-utils firefox gnome-vfs2
 
     sudo systemctl enable sddm.service
     sudo systemctl enable vboxservice.service
